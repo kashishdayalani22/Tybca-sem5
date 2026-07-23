57eced7c3cefb1526ec51eaf10589387b39f0893
kashishdayalani22 <kashishdayalani22@gmail.com>
2026-07-23 09:03:18 +0530

    Add files via upload

---
+++ DAA (a)/LAB1/Bubble_sort.java
@@ -0,0 +1,18 @@
+public class Bubble_sort {
+    public static void main(String[] args) {
+        int[] arr = {22,11,18,17,36};
+        for(int i=0; i<arr.length -1; i++){
+            for(int j=0; j<arr.length -1; j++){
+
+                if(arr[j] > arr[j +1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for(int j=0; j < arr.length; j++){
+            System.out.print(arr[j] + " , ");
+        }
+    }
+}
---
+++ DAA (a)/LAB1/Insertion_sort.java
@@ -0,0 +1,25 @@
+public class Insertion_sort {
+    public static void main(String[] args) {
+        int[] arr = {22, 10, 26, 8, 5};
+
+        
+        for (int i = 1; i < arr.length; i++) {   
+            int key = arr[i];                  
+            int j = i - 1;                      
+
+            
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            
+            arr[j + 1] = key;
+        }
+
+        
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
