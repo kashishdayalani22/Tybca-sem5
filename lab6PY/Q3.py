70960b0926fba6c10a07ee55c182f15acf1664d7
kashishdayalani22 <kashishdayalani22@gmail.com>
2026-08-27 11:02:24 +0530

    Add files via upload

---
+++ lab6PY/Q3.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+print("original array: ", arr)
+
+print("First element :", arr[0])
+print("Last element :", arr[-1])
+print("Third element :", arr[2])
+print("Length of array: ", len(arr))
---
+++ lab6PY/Q7.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+num = np.array([25, 10, 45, 30, 15])
+
+print(num)
