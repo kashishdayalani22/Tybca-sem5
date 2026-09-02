ac74b51aea4ab77b1f523609804d70f1ddbbbed1
kashishdayalani22 <kashishdayalani22@gmail.com>
2026-09-03 00:05:58 +0530

    Refactor Q4.java for improved readability

--- DAA (a)/Lab 4/Q4.java
+++ DAA (a)/Lab 4/Q4.java
@@ -1,13 +1,10 @@
 class Q4 {
 
     public static void main(String[] args) {
-
-        // Fixed 2x2 matrices
         int[][] A = {
             {1, 2},
             {3, 4}
         };
-
         int[][] B = {
             {5, 6},
             {7, 8}
@@ -15,7 +12,6 @@ class Q4 {
 
         int[][] C = new int[2][2];
 
-        // Strassen's 7 multiplications
         int M1 = (A[0][0] + A[1][1]) * (B[0][0] + B[1][1]);
         int M2 = (A[1][0] + A[1][1]) * B[0][0];
         int M3 = A[0][0] * (B[0][1] - B[1][1]);
@@ -24,13 +20,11 @@ class Q4 {
         int M6 = (A[1][0] - A[0][0]) * (B[0][0] + B[0][1]);
         int M7 = (A[0][1] - A[1][1]) * (B[1][0] + B[1][1]);
 
-        // Calculate result matrix
         C[0][0] = M1 + M4 - M5 + M7;
         C[0][1] = M3 + M5;
         C[1][0] = M2 + M4;
         C[1][1] = M1 - M2 + M3 + M6;
 
-        // Display result
         System.out.println("Resultant Matrix:");
 
         for (int i = 0; i < 2; i++) {
