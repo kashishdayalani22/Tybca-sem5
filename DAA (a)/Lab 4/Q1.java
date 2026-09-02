67910d57808781896c9bf170d29c8f2020abcd49
kashishdayalani22 <kashishdayalani22@gmail.com>
2026-09-03 00:34:25 +0530

    Refactor Q1.java to improve merge sort implementation

--- DAA (a)/Lab 4/Q1.java
+++ DAA (a)/Lab 4/Q1.java
@@ -1,38 +1,68 @@
-class Q1{
-    public static void main(String[] args){
-        int[] arr1 = {34, 7, 23, 32, 5, 62};
-        int[] arr2 = {14, 55, 3, 27, 18, 41};
+public class Q1{
+    public static void main(String[] args) {
+        int[] data1 = {38, 27, 43, 3, 9, 82, 10};
+        int[] data2 = {12, 11, 13, 5, 6, 7};
 
-        mergeSort(arr1, 0, arr1.length - 1);
-        mergeSort(arr2, 0, arr2.length - 1);
+        mergeSort(data1, 0, data1.length - 1);
+        mergeSort(data2, 0, data2.length - 1);
 
+        printArray(data1);
+        printArray(data2);
     }
 
-    public static void mergeSort(int[] arr, int left, int right){
-        if (left < right){
-            int mid = (left + right) / 2;
-
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
             mergeSort(arr, left, mid);
             mergeSort(arr, mid + 1, right);
-
             merge(arr, left, mid, right);
         }
     }
 
-    public static void merge(int[] arr, int left, int mid, int right){
+    public static void merge(int[] arr, int left, int mid, int right) {
         int n1 = mid - left + 1;
         int n2 = right - mid;
 
         int[] L = new int[n1];
         int[] R = new int[n2];
 
-        for(int i = 0; i < n1; i++){
+        for (int i = 0; i < n1; ++i) {
             L[i] = arr[left + i];
         }
-
-        for(int j = 0; j < n2; j++){
+        for (int j = 0; j < n2; ++j) {
             R[j] = arr[mid + 1 + j];
         }
 
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
